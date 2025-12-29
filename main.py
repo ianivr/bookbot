3e7899d5b9dbf8639eb62b0582fa20e2199c936a
ianivr <ianivrubinstein@gmail.com>
2025-12-29 17:46:09 +0000

    add new functions and reestructure code output

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from stats import get_num_words, get_char_count
+from stats import get_num_words, get_char_count, get_sorted_dictionary, remove_non_alpha_chars, get_formated_list
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    print(f"Found {get_num_words(get_book_text("books/frankenstein.txt"))} total words")
+    path = "books/frankenstein.txt"
+    book = get_book_text(path)
+    result = get_char_count(book)
+    chars_list = get_sorted_dictionary(result)
+    new_list = remove_non_alpha_chars(chars_list)
+    formated_list = get_formated_list(new_list)
 
-    result = get_char_count(get_book_text("books/frankenstein.txt"))
-    print(result)
+    string_structure = f"""
+    ============ BOOKBOT ============
+    Analyzing book found at {path}...
+    ----------- Word Count ----------
+    Found {get_num_words(book)} total words
+    --------- Character Count -------
+    {formated_list}
+    ============= END ===============
+    """
 
+    print(string_structure)
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,37 @@ def get_char_count(text):
             char_counter[lowerChar] = 1
     
     return char_counter
+
+def get_sorted_dictionary(char_dict):
+    chars_list = []
+
+    for char, count in char_dict.items():
+        chars_list.append({
+            "char": char,
+            "num": count
+        })
+
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
+
+def sort_on(dict_item):
+    return dict_item["num"]
+
+def remove_non_alpha_chars(chars_list):
+    new_list = []
+    for item in chars_list:
+        if item["char"].isalpha():
+            new_list.append(item)
+    
+    return new_list
+
+def get_formated_list(old_list):
+    formated_string = ""
+
+    for item in old_list:
+        if not formated_string:
+            formated_string += f"{item["char"]}: {item["num"]}"
+        else:
+            formated_string += f"\n    {item["char"]}: {item["num"]}"
+
+    return formated_string
